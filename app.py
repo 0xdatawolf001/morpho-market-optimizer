76f544c8a6e85a634f3e8a2a014f14619d7544fa
0xdatawolf001 <0xdatawolf@gmail.com>
2026-01-16 19:41:38 +0800

    UI: Remove From and to token in market rebalance steps

--- app.py
+++ app.py
@@ -1826,11 +1826,9 @@ if not df_selected.empty:
                     "From": src['name'],
                     "From Market ID": src.get('id', 'N/A')[:7],
                     "From (Chain)": src['chain'],
-                    "From (Token)": src['token'],
                     "To": dst['name'],
                     "To Market ID": dst.get('id', 'N/A')[:7],
                     "To (Chain)": dst['chain'],
-                    "To (Token)": dst['token'],
                     "Amount to move ($)": amount_to_move,
                     "Remaining Funds In Source ($)": src['running_balance'],
                     "Operation Type": op_type,
@@ -1861,10 +1859,8 @@ if not df_selected.empty:
                     "Ordering": ordering_counter,
                     "From": src['name'],
                     "From (Chain)": src['chain'],
-                    "From (Token)": src['token'],
                     "To": "Wallet (Unallocated)",
                     "To (Chain)": "Wallet",
-                    "To (Token)": "CASH",
                     "Amount to move ($)": src['available'],
                     "Remaining Funds In Source ($)": src['running_balance'],
                     "Operation Type": "Cash Out",
@@ -1897,7 +1893,7 @@ if not df_selected.empty:
                     "Amount to move ($)": "${:,.2f}",
                     "Remaining Funds In Source ($)": "${:,.2f}"
                 }).applymap(highlight_op, subset=['Operation Type']).applymap(lambda x: 'color: red' if x < 0.01 else '', subset=['Remaining Funds In Source ($)']), 
-                column_order=["Ordering", "Operation Type", "From", "From Market ID", "From (Chain)", "From (Token)", "To", "To Market ID", "To (Chain)", "To (Token)", "Amount to move ($)", "Remaining Funds In Source ($)"],
+                column_order=["Ordering", "Operation Type", "From", "From Market ID", "From (Chain)", "To", "To Market ID", "To (Chain)", "Amount to move ($)", "Remaining Funds In Source ($)"],
                 width='stretch', 
                 hide_index=True
             )
